9479c797de71ffb0dc65d4e1aa1426aa3d84aaf4
Farhan Kaioum <km1472233@gmail.com>
2020-09-21 23:10:54 -0700

    routing, navbar added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
+import {AddRestoComponent} from './add-resto/add-resto.component'
+import {UpdateRestoComponent} from './update-resto/update-resto.component'
+import {ListRestoComponent} from './list-resto/list-resto.component'
+import {LoginComponent} from './login/login.component'
+import {RegistrationComponent} from './registration/registration.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"add",
+    component:AddRestoComponent
+  },
+  {
+    path:"update",
+    component:UpdateRestoComponent
+  },
+  {
+    path:"list",
+    component:ListRestoComponent
+  },
+  {
+    path:"login",
+    component:LoginComponent
+  },
+  {
+    path:"register",
+    component:RegistrationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { UpdateRestoComponent } from './update-resto/update-resto.component';
 import { ListRestoComponent } from './list-resto/list-resto.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { RegistrationComponent } from './registration/registration.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
